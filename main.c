28b57284c9292fd9a91eb2333cb6e19f764d7f70
ramin sanei <raminsanei1992@gmail.com>
2024-06-10 17:20:12 +0200

    sort stack and init_stack_indexes added

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/27 10:30:36 by ssanei            #+#    #+#             */
-/*   Updated: 2024/06/09 18:53:10 by marvin           ###   ########.fr       */
+/*   Created: 2024/05/31 15:27:46 by ssanei            #+#    #+#             */
+/*   Updated: 2024/06/10 17:20:11 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,11 +41,23 @@ void	ft_free_stack(t_list **stack)
 	free(stack);
 }
 
+void	sort_stack(t_list **stackA, t_list **stackB)
+{
+	if (ft_listsize(*stackA) == 2)
+		swap(stackA, NULL, "sa");
+	else if (ft_listsize(*stackA) == 3)
+		sort_3(stackA);
+	else if (ft_listsize(*stackA) <= 5)
+		sort_5(stackA, stackB);
+	else
+		sort_bigger(stackA, stackB);
+}
+
 void stacks_allocation(t_list **stackA, t_list **stackB, int argc, char *argv[]) {
     *stackA = (t_list *)malloc(sizeof(t_list));
     *stackB = (t_list *)malloc(sizeof(t_list));
     initial_Stack(stackA, argc, argv);
-		// init_stack_indexes(stackA);
+	init_stack_indexes(stackA);
 
 }
 
@@ -67,6 +79,8 @@ int	main(int argc, char *argv[])
 		ft_free_stack(&stackB);
 		return (0);
 	}
-	
+	sort_stack(&stackA, &stackB);
+	ft_free_stack(&stackA);
+	ft_free_stack(&stackB);
 	return (0);
 }
